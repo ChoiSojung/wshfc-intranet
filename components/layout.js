d747e4f7b10cb7e184a0ab3b4715cd0ba5f3d98c
Sojung Choi <sojc312gmail.com>
2021-04-22 15:36:43 -0700

    added search to nav and styling

--- components/layout.js
+++ components/layout.js
@@ -19,28 +19,36 @@ export default function Layout({children, home}) {
     <div>
         <body className="antialiased bg-gray-200">
             
-            <header className="lg:px-16 px-6 bg-blue-900 flex flex-wrap items-center lg:py-0 py-2">
+            <header className="lg:px-16 px-6 bg-blue-900 h-20 flex flex-wrap items-center lg:py-0 py-2">
                 <div className="flex-1 flex justify-between items-center">
                     
                     <Link href="/">
-                            <a className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
-                                <Image
-                                    src="/../public/images/logo.jpg"
-                                    alt="name" 
-                                    width={36}
-                                    height={36}
-                                />
-                            </a>
-                        </Link>
+                        <a className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+                            <Image
+                                src="/../public/images/logo2.png"
+                                alt="name" 
+                                width={175}
+                                height={55}
+                            />
+                        </a>
+                    </Link>
 
                 </div>
+                <div className="flex-1 flex justify-between items-center">
+                    <form className="w-full px-4">
+                        <label className="hidden" for="search-form">Search</label>
+                        <input className="bg-grey-lightest border-2 focus:border-orange p-2 rounded-lg shadow-inner w-full" placeholder="Search" type="text"/>
+                            <button className="hidden">Submit</button>
+                    </form>
+                </div>
+             
 
                 <label for="menu-toggle" className="pointer-cursor lg:hidden block"><svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
                 <input className="hidden" type="checkbox" id="menu-toggle" />
 
                 <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
                     <nav>
-                        <ul className="lg:flex items-center justify-between text-base text-white pt-4 lg:pt-0">
+                        <ul className="lg:flex items-center justify-between text-base text-white font-serif font-semibold text-3x1 pt-4 lg:pt-0">
                         <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">About</a></li>
                         <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="hr-finance">HR &amp; Finance</a></li>
                         <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">IT</a></li>
@@ -51,15 +59,17 @@ export default function Layout({children, home}) {
                     </nav>
                    
                 </div>
+  
 
             </header>
+            <Sidenav></Sidenav>
             <div className="px-4">
                 <div className="max-w-3xl bg-white rounded-lg mx-auto my-16 p-16">
                     <h1 className="text-2xl font-medium mb-2">Let's Build: With Tailwind CSS</h1>
                     <h2 className="font-medium text-sm text-indigo-400 mb-4 uppercase tracking-wide">Responsive Navbar</h2>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum illo cupiditate molestias atque consequuntur ea quo cumque, 
                     odit velit sint similique? Asperiores ratione aperiam tempora, alias corrupti deleniti quaerat molestiae.
-                    
+                  
                     <span>
                         
                         {children}
