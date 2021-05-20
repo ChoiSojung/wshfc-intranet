9938b5d6ed87b6046bc77031cd85e9ff16a8134c
Sojung Choi <sojc312gmail.com>
2021-05-20 12:44:39 -0700

    cleaning up unused components

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,5 @@
-import Layout from '../components/layout'
-import Carousel from '../components/carousel'
-import ContentBlock from '../components/contentblock'
 import Image from 'next/image'
 import Link from 'next/link'
-import Sidenav from '../components/sidenav'
 
 export default function About(){
 
