ce977db49ba8b0be45e113555603f5f6f7995e1b
Sojung Choi <sojc312gmail.com>
2021-05-21 13:17:06 -0700

    small updates

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -26,8 +26,8 @@ export default function ScrollToTop() {
         <div className="scroll-to-top">
             {isVisible &&
                 <div onClick={scrollToTop}>
-                    <button className="rounded-lg bg-blue-500"><svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
+                    <button className="rounded-lg bg-blue-500"><svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
                     </svg></button>
                 </div>}
         </div>
