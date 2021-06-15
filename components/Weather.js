deb0a1c716af514cc421f953c01b937a90438dca
Sojung Choi <sojc312gmail.com>
2021-06-15 14:13:25 -0700

    weather api call works - is returning data

--- components/Weather.js
+++ components/Weather.js
@@ -8,17 +8,24 @@ export default function Weather(){
     const [data, setData] = useState([])
 
     useEffect(()=> {
-        navigator.geolocation.getCurrentPosition(function(position){
-            setLat(position.coords.latitude)
-            setLong(position.coords.longitude)
-        })
+        const fetchData= async() => { 
+            navigator.geolocation.getCurrentPosition(function(position){
+                setLat(position.coords.latitude)
+                setLong(position.coords.longitude)
+            })
 
-        console.log('Latitude is: ', lat)
-        console.log('Longitude is: ', long)
+            await fetch(`${process.env.WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&units=imperial&appid=${process.env.WEATHER_API_KEY}`)
+                .then(res => res.json())
+                .then(result => {
+                    setData(result)
+                    console.log(result)
+            })
+        } 
+        fetchData()
     }, [lat, long])
 
-    async function getWeather() {
-        await fetch(`${process.env.WEATHER_API_URL}/weather/?lat=${lat}&lon=${long}&units=imperial&AAPID=${process.env.WEATHER_API_KEY}`)
+    /* async function getWeather() {
+        await fetch(`${process.env.WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&units=imperial&appid=${process.env.WEATHER_API_KEY}`)
             .then(res => res.json())
             .then(result => {
                 setData(result)
@@ -26,17 +33,17 @@ export default function Weather(){
         })
          
     }
-    getWeather()
+    getWeather() */
         
     return (
         <div>
             <Card>
                 <Card.Content>
-                    <Card.header className="font-serif">{data.name}</Card.header>
-                    <p>Temperature: {data.main.temp}</p>
+                    {/* <Card.header className="font-serif">{data.name}</Card.header> */}
+                    {/* <p>Temperature: {data.main.temp}</p>
                     <p>Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
                     <p>Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-                    <p>Description: {data.weather[0].description}</p>
+                    <p>Description: {data.weather[0].description}</p> */}
                     <p>Day: {moment().format('dddd')}</p>
                     <p>Date: {moment().format('LL')}</p>
                 </Card.Content>
